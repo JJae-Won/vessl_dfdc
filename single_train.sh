a73fc421bf188209a9c6724bdaed47169931bfa1
JJae-Won <jjwopal@gmail.com>
2023-05-24 13:51:49 +0900

    install apex

--- single_train.sh
+++ single_train.sh
@@ -1,3 +1,7 @@
+git clone https://github.com/NVIDIA/apex
+sed -i 's/check_cuda_torch_binary_vs_bare_metal(torch.utils.cpp_extension.CUDA_HOME)/pass/g' apex/setup.py
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext"  ./apex
+
 python training/pipelines/train_classifier_vessl.py \
     --config configs/b7.json \
      --freeze-epochs 0 --test_every 1 --opt-level O1 \
